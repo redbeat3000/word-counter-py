9d05e1a0828bd311e86aaa466f37ecb345c92aaf
Sukuna Divine <155563748+redbeat3000@users.noreply.github.com>
2025-02-05 12:22:22 +0300

    Add files via upload

---
+++ word_count.py
@@ -0,0 +1,8 @@
+#the program below counts the number of words that are written
+a = input("Type in a couple words:")
+
+#length of paragraph
+x = len(a)
+
+#print length
+print(f"The number of charaters in 'a' are {x} !")
